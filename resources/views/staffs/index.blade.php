601efd50db3e1f50fbc63f9311234461f5cc58a7
yethuaungptu <yethuaung481995@gamil.com>
2019-07-29 15:40:29 +0630

    add staff delete sweet alert

--- resources/views/staffs/index.blade.php
+++ resources/views/staffs/index.blade.php
@@ -37,8 +37,36 @@
                             <form action="{{ route('staffs.destroy', $staff->id)}}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button class="btn btn-danger" type="submit">Delete</button>
+                                <button class="btn btn-danger" type="button" onclick="ch(this.form, {{ \App\Order::where('staff_id',$staff->id)->count() }})">Delete</button>
                             </form>
+                            <script>
+                                function ch(f,id){
+                                    swal({
+                                            title: "Are you sure?",
+                                            text: (id > 0)? "This staff id use "+ id +" count(s) in Order. You can not be delete this imaginary staff!" : "You will not be able to recover this imaginary staff!",
+                                            type: "warning",
+                                            showCancelButton: true,
+                                            confirmButtonColor: "#DD6B55",
+                                            confirmButtonText: (id == 0)?"Yes, delete it!":"Ok, I Know",
+                                            cancelButtonText: "No, cancel plx!",
+                                            closeOnConfirm: false,
+                                            closeOnCancel: false,
+                                            confirmButtonColor: "#63A8EB"
+                                        },
+                                        function (isConfirm) {
+                                            if (isConfirm) {
+                                                if (id ==  0){
+                                                    swal("Deleted!", "Your imaginary staff has been deleted.", "success");
+                                                    f.submit();
+                                                }
+                                                swal("Ok!", "Your imaginary staff deleting process has been canceled.", "success");
+                                            } else {
+                                                swal("Cancelled", "Your imaginary menu is safe :)", "error");
+                                            }
+                                        });
+
+                                }
+                            </script>
                         </td>
                     </tr>
                 @endforeach
